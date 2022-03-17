566d021f3f9993a70638aef89589142c2e50bb60
Spencer <Spaopaopao@gmail.com>
2022-03-16 20:37:50 -0400

    Addings jpg to gif exec.

--- clean_data.py
+++ clean_data.py
@@ -6,8 +6,15 @@ from util import data_processing
 
 
 def main():
-    data_processing.file_to_png(source_directory='data/train/abstract/',
-                                target_directory='data/cleaned_train/abstract/')
+    """Converting JPGs to PNGs as input for stylegan2 on google colab."""
+    # data_processing.file_to_png(source_directory='data/train/abstract/',
+    #                             target_directory='data/cleaned_train/abstract/')
+    """Use the results of the model training ouput for each 20th kimg -> GIF."""
+    data_processing.jpg_to_gif(file_path_input='data/Abstract_Progression_Images/',
+                               file_path_output='data/Abstract_GIF/',
+                               genre='abstract',
+                               extend_frames=True,
+                               duration=0.5)
 
 
 if __name__ == '__main__':
