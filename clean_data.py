56c10ad8b413fe48451d8fe66604c833c389baa8
Spencer <Spaopaopao@gmail.com>
2022-03-20 14:28:25 -0400

    Updating files most up to date

--- clean_data.py
+++ clean_data.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 """
     Executes utility scripts to ensure clean data for input.
+    This is done iteratively. Not automated.
 """
 from util import data_processing
 
 
 def main():
     """Converting JPGs to PNGs as input for stylegan2 on google colab."""
-    # data_processing.file_to_png(source_directory='data/train/abstract/',
-    #                             target_directory='data/cleaned_train/abstract/')
+    # data_processing.file_to_png(source_directory='data/train/',
+    #                             target_directory='data/cleaned_train/')
+    """Padding images to square shape and resize if appropriate."""
+    data_processing.padd_resize_for_input(
+        image_address='data/cleaned_train/',
+        dimension=1024,
+        target_address='data/cleaned_train_resized/')  # cleaned_train_resized
+    """Checking number of channels."""
+    arr = data_processing.check_channels(file_path_address='data/cleaned_train_resized',
+                                         image_type='png')
+    print(arr)  # if empty, go ahead and upload data to google cloud. Else address images.
+
     """Use the results of the model training ouput for each 20th kimg -> GIF."""
-    data_processing.jpg_to_gif(file_path_input='data/Abstract_Progression_Images/',
-                               file_path_output='data/Abstract_GIF/',
-                               genre='abstract',
-                               extend_frames=True,
-                               duration=0.5)
+    # data_processing.jpg_to_gif(file_path_input='data/Abstract_Progression_Images/',
+    #                            file_path_output='data/Abstract_GIF/',
+    #                            genre='abstract',
+    #                            extend_frames=True,
+    #                            duration=0.5)
 
 
 if __name__ == '__main__':
--- util/data_processing.py
+++ util/data_processing.py
@@ -5,6 +5,8 @@
 from glob import glob
 import cv2
 import imageio
+from PIL import Image
+from typing import Tuple
 
 
 def file_to_png(source_directory: str,
@@ -22,7 +24,7 @@ def file_to_png(source_directory: str,
     jpgs = glob(f'{source_directory}*.jpg')
     for j in jpgs:
         img = cv2.imread(j)
-        img = cv2.resize(img, (256, 256), cv2.INTER_AREA)
+        # img = cv2.resize(img, (256, 256), cv2.INTER_AREA)
         cv2.imwrite(f'{target_directory}/' + (j[:-3] + 'png').split('\\')[1], img)
 
 
@@ -54,3 +56,81 @@ def jpg_to_gif(file_path_input: str,
     for filename in filenames:
         images.append(imageio.imread(filename))
     imageio.mimsave(f'{file_path_output}{genre}.gif', images, duration=0.5)
+
+
+def padd_resize_for_input(image_address: str,
+                          dimension: int,
+                          target_address: str) -> None:
+    """Padd and resized PNG images for any specified dimensions
+    Parameters:
+    ----------
+    image_address: str
+        location of where all the pngs are located.
+    dimension: int
+        What square dimension you want to scale toward.
+    target_address: str
+        Where to save the resulting resized, padded image.
+    Return:
+    ----------
+    None
+    """
+    filenames = glob(f'{image_address}*.png')
+    print("Padding and resizing PNG images...")
+    for f in filenames:
+        res = _expand2square(f, (255, 255, 255)).resize((1024, 1024))
+        fi = f.split('\\')[1]
+        res.save(f'{target_address}/{fi}', quality=95)
+
+
+def _expand2square(image_path: str, background_color: Tuple[int, int, int]):
+    """Helper function to padd_resize_for_input. This function resizes images to a square.
+    Parameters:
+    ----------
+    image_path: str
+        Location of where individual images are located.
+    background color: [int,int,int]
+        Color for padding. (Currently using white)
+    Return:
+    ----------
+    None
+    """
+    pil_img = Image.open(image_path)
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
+
+
+def check_channels(file_path_address: str, image_type: str):
+    """Manual verifier to determine which images to further clean or remove.
+    This checks to see if there is a consistent third dimension in each of the images.
+
+    Paramters:
+    ---------
+    file_path_address: str
+        Address of where all jpgs are located.
+    image_type: str
+        image type as in .png or .jpg
+    Return:
+    ---------
+    Array of name of jpgs to address.
+    """
+    imgs = glob(f'{file_path_address}*.{image_type}')
+    arr_other = []
+    for i, j in enumerate(imgs):
+        print(f"Starting {i} for filename: {j}")
+        im = cv2.imread(j)
+        try:
+            if im.shape[2] != 3:
+                arr_other.append(j)
+        except Exception as e:
+            arr_other.append(j)
+            print(e)
+    return arr_other
