9a5e494c5f224306024d950eafd48a6126733e45
Spencer <Spaopaopao@gmail.com>
2022-03-20 14:28:47 -0400

    uncommenting

--- clean_data.py
+++ clean_data.py
@@ -11,14 +11,14 @@ def main():
     # data_processing.file_to_png(source_directory='data/train/',
     #                             target_directory='data/cleaned_train/')
     """Padding images to square shape and resize if appropriate."""
-    data_processing.padd_resize_for_input(
-        image_address='data/cleaned_train/',
-        dimension=1024,
-        target_address='data/cleaned_train_resized/')  # cleaned_train_resized
+    # data_processing.padd_resize_for_input(
+    #     image_address='data/cleaned_train/',
+    #     dimension=1024,
+    #     target_address='data/cleaned_train_resized/')  # cleaned_train_resized
     """Checking number of channels."""
-    arr = data_processing.check_channels(file_path_address='data/cleaned_train_resized',
-                                         image_type='png')
-    print(arr)  # if empty, go ahead and upload data to google cloud. Else address images.
+    # arr = data_processing.check_channels(file_path_address='data/cleaned_train_resized',
+    #                                      image_type='png')
+    # print(arr)  # if empty, go ahead and upload data to google cloud. Else address images.
 
     """Use the results of the model training ouput for each 20th kimg -> GIF."""
     # data_processing.jpg_to_gif(file_path_input='data/Abstract_Progression_Images/',
